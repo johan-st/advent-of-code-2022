86ab96f29dd6753470ec61f56da91c284dcd73f2
Johan Strand <53336235+johan-st@users.noreply.github.com>
2022-12-04 23:50:38 +0100

    solve day 1

--- 1/main.go
+++ 1/main.go
@@ -1,9 +1,70 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"sort"
+	"strconv"
+	"strings"
+)
 
+func main() {
+	fmt.Println("running...")
+	input := loadInput()
+	splitInput := splitInput(input)
+	calories := []int{}
+	for _, value := range splitInput {
+		calories = append(calories, sumString(value))
+	}
+
+	sort.Ints(calories)
+	fmt.Println("Three largest are: ", calories[len(calories)-1], calories[len(calories)-2], calories[len(calories)-3])
+	fmt.Println("Total: ", calories[len(calories)-1]+calories[len(calories)-2]+calories[len(calories)-3])
+}
+
+// read input from file
+func loadInput() string {
+	data, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return ""
+	}
+	return string(data)
 }
 
 func splitInput(input string) []string {
-	return nil
+	if input == "" {
+		return nil
+	}
+	return strings.Split(input, "\r\n\r\n")
+}
+
+func sumString(input string) int {
+	if input == "" {
+		return 0
+	}
+	split := strings.Split(input, "\r\n")
+	sum := 0
+	for _, value := range split {
+		num, err := strconv.Atoi(value)
+		if err != nil {
+			fmt.Println(err)
+			return 0
+		}
+		sum += num
+	}
+	return sum
+}
+
+func getMax(input []int) int {
+	if len(input) == 0 {
+		return 0
+	}
+	max := input[0]
+	for _, value := range input {
+		if value > max {
+			max = value
+		}
+	}
+	return max
 }
--- 1/main_test.go
+++ 1/main_test.go
@@ -15,11 +15,35 @@ func Test_splitInput(t *testing.T) {
 		want []string
 	}{
 		{"empty", args{""}, nil},
+		{"one elf", args{"500\n500\n100"}, []string{"500\n500\n100"}},
+		{"two elf", args{"500\n500\n100\n\n500\n100"}, []string{"500\n500\n100", "500\n100"}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := splitInput(tt.args.input); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("splitInput() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_sumString(t *testing.T) {
+	type args struct {
+		in0 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"empty case", args{""}, 0},
+		{"one elf", args{"500\n500\n100"}, 1100},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sumString(tt.args.in0); got != tt.want {
+				t.Errorf("sumString() = %v, want %v", got, tt.want)
 			}
 		})
 	}
