d60b426520c3a3a92e6cc543dd22409c4030e231
Johan Strand <53336235+johan-st@users.noreply.github.com>
2022-12-07 23:56:26 +0100

    prep day 8

---
+++ 8/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	u "github.com/johan-st/advent-of-code-2022/util"
+)
+
+func main() {
+	// Read input
+	input := u.Load("sample.txt")
+	fmt.Println(input)
+
+	// part 1
+
+	// part 2
+
+}
