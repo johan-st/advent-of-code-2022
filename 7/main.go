fd7866f24d6b67820d1c914364a1b35c0caae325
Johan Strand <53336235+johan-st@users.noreply.github.com>
2022-12-07 23:04:15 +0100

    start work on day 7

---
+++ 7/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	u "github.com/johan-st/advent-of-code-2022/util"
+)
+
+func main() {
+	// Read input
+	input := strings.Split(u.Load("sample.txt"), "\r\n")
+
+	// parse into filesystem
+	fs := newDir("/")
+	pos := []string{}
+	for _, line := range input {
+		// split line into path and size
+		words := strings.Split(line, " ")
+
+		if words[0] == "$" && words[1] == "cd" && words[2] == "/" {
+			pos = []string{}
+		} else if words[0] == "$" && words[1] == "cd" && words[2] == ".." {
+			pos = pos[:len(pos)-1]
+		} else if words[0] == "$" && words[1] == "cd" {
+			pos = append(pos, words[2])
+		} else if words[0] == "$" && words[1] == "ls" {
+			continue
+		} else if words[0] == "dir" {
+			fs.at(pos).addDir(words[1])
+		} else {
+			// is file
+			size, err := strconv.Atoi(words[0])
+			if err != nil {
+				panic(err)
+			}
+			fs.at(pos).addFile(words[1], size)
+		}
+
+	}
+
+	// Print filesystem
+	fmt.Printf("_________OUTPUT_________\n%vTOTAL SIZE: %d", fs, fs.size())
+
+	// Find total size of directories less than 100000 in size
+
+	// Print total size
+}
+
+func (d dir) at(pos []string) dir {
+	current := d
+	for _, p := range pos {
+		current = current.dirs[p]
+	}
+
+	return current
+}
+
+func newDir(name string) dir {
+	return dir{name: name, dirs: map[string]dir{}, files: map[string]int{}}
+}
+
+// kind is either "dir" or "file"
+type dir struct {
+	name  string
+	dirs  map[string]dir
+	files map[string]int
+}
+
+func (d dir) addDir(name string) {
+	d.dirs[name] = newDir(name)
+}
+
+func (d dir) addFile(name string, size int) {
+	d.files[name] = size
+}
+
+func (d dir) size() int {
+	size := 0
+	for _, s := range d.files {
+		size += int(s)
+	}
+	for _, d := range d.dirs {
+		size += d.size()
+	}
+	return size
+}
+
+func (d dir) String() string {
+	return d.toString(0)
+}
+
+func (d dir) toString(indent int) string {
+	s := fmt.Sprintf("%s- %s (dir)\n", indentString(indent), d.name)
+	for name, size := range d.files {
+		s += fmt.Sprintf("%s- %s (file, size=%d)\n", indentString(indent+1), name, size)
+	}
+	for _, d := range d.dirs {
+		s += d.toString(indent + 1)
+	}
+	return s
+}
+
+func indentString(indent int) string {
+	s := ""
+	for i := 0; i < indent; i++ {
+		s += "  "
+	}
+	return s
+}
