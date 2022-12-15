6db33c8cf1f73a849172eaa7f33c62c07b1c8b86
Johan Strand <53336235+johan-st@users.noreply.github.com>
2022-12-15 05:02:42 +0100

    solve day 10 part 2

--- 10/main.go
+++ 10/main.go
@@ -13,55 +13,77 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	ms := []int{20, 60, 100, 140, 180, 220}
-	sum := 0
-	for _, m := range ms {
-		localSum := signalAt(ins, m)
-		sum += localSum
-		fmt.Printf("%d: %d\n", m, localSum)
+
+	crt := crtScreen{}
+	c := cpu{ins: ins, registry: 1}
+	for i := 0; i < 240; i++ {
+		crt.update(c)
+		c.tick()
+		// fmt.Println(c)
 	}
-	// fmt.Println(ins)
-	fmt.Printf("SUM: %d\n", sum)
+	fmt.Println(crt)
+}
+
+type crtScreen struct {
+	rows [6][40]rune
+}
+
+func (crt *crtScreen) update(c cpu) {
+	line := c.cycle / 40
+	char := c.cycle % 40
+
+	if c.registry >= char-1 && c.registry <= char+1 {
+		crt.rows[line][char] = 'X'
+	} else {
+		crt.rows[line][char] = '.'
+	}
 
 }
 
-func signalAt(ins instructions, cycles int) int {
-	c := cpu{ins: ins, registry: 1, cycle: 0}
-	return c.run(cycles)
+func (crt crtScreen) String() string {
+	str := "------------------------------------------\n"
+	for _, row := range crt.rows {
+		str += "|"
+		for _, ru := range row {
+			if string(ru) != "\x00" {
+				str += string(ru)
+			} else {
+				str += "."
+			}
+		}
+		str += "|\n"
+	}
+	str += "------------------------------------------"
+	return str
 }
 
 type cpu struct {
-	ins      instructions
-	registry int
-	cycle    int
+	ins                instructions
+	registry           int
+	cycle              int
+	cycleOnInstruction int
+	currentInstruction int
 }
 
-func (c cpu) run(measure int) int {
-	for _, i := range c.ins {
-		if i.addx {
-			c.cycle++
-			if c.cycle >= measure {
-				return c.signal()
-			}
-			c.cycle++
-			if c.cycle >= measure {
-				return c.signal()
-			}
-			c.registry += i.value
-			// fmt.Println(c.registry-i.value, "+", i.value, c.registry)
-		} else {
-			c.cycle++
-			if c.cycle >= measure {
-				return c.signal()
-			}
-		}
+func (c *cpu) tick() {
+	c.cycle++
+	if c.ins[c.currentInstruction].addx && c.cycleOnInstruction == 0 {
+		c.cycleOnInstruction++
+	} else if c.ins[c.currentInstruction].addx && c.cycleOnInstruction == 1 {
+		c.registry += c.ins[c.currentInstruction].value
+		c.currentInstruction++
+		c.cycleOnInstruction = 0
+	} else {
+		c.currentInstruction++
 	}
-	return 0
 }
 
 func (c cpu) signal() int {
 	fmt.Println(c.cycle, c.registry, c.cycle*c.registry)
 	return c.cycle * c.registry
+}
+func (c cpu) String() string {
+	return fmt.Sprintf("--CPU--\ncycle: %d\nins: %d of %d\nreg:%d\nregCycle:%d\n-------\n", c.cycle, len(c.ins), c.currentInstruction, c.registry, c.cycleOnInstruction)
 }
 
 type instruction struct {
