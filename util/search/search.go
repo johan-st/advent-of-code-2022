30d5ec0153916bed717f658ab1581cdad207fea2
Johan Strand <53336235+johan-st@users.noreply.github.com>
2022-12-18 23:27:25 +0100

    create search util (bfs)

--- util/search/search.go
+++ util/search/search.go
@@ -1,20 +1,171 @@
 package search
 
-import "fmt"
+import (
+	"fmt"
+)
 
-type graph map[int]node
+type Graph map[int]node
 
 type node struct {
+	id          int
 	neighbours  []int
-	distance    int
 	predecessor int
 }
 
-func (g graph) getNeighboursOf(id int) ([]int, error) {
-	if node, ok := g[id]; ok {
-		return node.neighbours, nil
-	} else {
-		return []int{}, fmt.Errorf("id %d is not in this graph", id)
+// edge {from, to} if used as a directed edge (as opposed to undirected)
+type Edge [2]int
+
+func (g Graph) AddNodes(ids []int) error {
+	for _, id := range ids {
+		err := g.addNode(id)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (g Graph) addNode(id int) error {
+	if id < 1 {
+		return fmt.Errorf("id must be greater than 0. id given was %d", id)
+
+	}
+	if _, ok := g[id]; ok {
+		return fmt.Errorf("node with id %d already exists", id)
+	}
+	g[id] = node{id: id}
+	return nil
+}
+
+func (g Graph) addEdge(e Edge) error {
+	if e[0] < 1 && e[1] < 1 {
+		return fmt.Errorf("could not add edge between node %d and %d. Ids must be greater than 0", e[0], e[1])
 	}
 
+	var n1, n2 node
+	if node, ok := g[e[0]]; !ok {
+		return fmt.Errorf("could not add edge between node %d and %d. %d does not exist", e[0], e[1], e[0])
+	} else {
+		n1 = node
+	}
+	if node, ok := g[e[1]]; !ok {
+		return fmt.Errorf("could not add edge between node %d and %d. %d does not exist", e[0], e[1], e[1])
+	} else {
+		n2 = node
+	}
+
+	n1.neighbours = append(g[e[0]].neighbours, e[1])
+	n2.neighbours = append(g[e[1]].neighbours, e[0])
+	g[e[0]] = n1
+	g[e[1]] = n2
+	return nil
+}
+
+func (g Graph) AddEdges(es []Edge) error {
+	for _, e := range es {
+		err := g.addEdge(e)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (g Graph) GetNeighboursFor(id int) []int {
+	node := g[id]
+	return node.neighbours
+}
+
+func (g Graph) addEdge_directional(e Edge) error {
+	if e[0] < 1 && e[1] < 1 {
+		return fmt.Errorf("could not add edge between node %d and %d. Ids must be greater than 0", e[0], e[1])
+	}
+
+	var n1 node
+	if node, ok := g[e[0]]; !ok {
+		return fmt.Errorf("could not add edge between node %d and %d. %d does not exist", e[0], e[1], e[0])
+	} else {
+		n1 = node
+	}
+
+	n1.neighbours = append(g[e[0]].neighbours, e[1])
+	g[e[0]] = n1
+	return nil
+}
+
+func (g Graph) AddEdges_directional(es []Edge) error {
+	for _, e := range es {
+		err := g.addEdge_directional(e)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (g Graph) BredthFirst(start int, end int) []int {
+	q := que{}
+	q.enque(g[start])
+	for !q.empty() {
+		current := q.deque()
+		for _, n := range current.neighbours {
+			next := g[n]
+			if n == end {
+				// TODO:
+				node := g[n]
+				node.predecessor = current.id
+				g[n] = node
+				q = que{}
+				break
+			} else if next.predecessor == 0 { //predecessor should only be 0 for not visited nodes
+				next.predecessor = current.id
+				g[n] = next
+				q.enque(next)
+			}
+		}
+	}
+
+	path := []int{end}
+	last := end
+	done := false
+	for !done {
+		id := g[last].predecessor
+		last = id
+		path = append(path, g[last].id)
+		if id == start {
+			done = true
+		}
+		if id < 1 {
+			fmt.Printf("something went very wrong here %d", id)
+			break
+		}
+	}
+	path = reverse(path)
+	return path
+}
+
+func reverse(ints []int) []int {
+	rev := []int{}
+	for i := range ints {
+		rev = append(rev, ints[len(ints)-1-i])
+	}
+	return rev
+}
+
+// Que
+type que []node
+
+func (q *que) enque(n node) {
+	*q = append(*q, n)
+}
+func (q *que) deque() node {
+	first := (*q)[0]
+	*q = (*q)[1:]
+	return first
+}
+func (q *que) empty() bool {
+	return len(*q) == 0
+}
+func (q *que) peek() node {
+	return (*q)[0]
 }
--- util/search/search_test.go
+++ util/search/search_test.go
@@ -1,37 +1,131 @@
-package search
+package search_test
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
+
+	s "github.com/johan-st/advent-of-code-2022/util/search"
 )
 
-func Test_graph_getNeighboursOf(t *testing.T) {
-	type args struct {
-		id int
+func Test_Graph_AddNodes(t *testing.T) {
+	ids := []int{1, 2, 3, 4, 50}
+	g := s.Graph{}
+	err := g.AddNodes(ids)
+	if err != nil {
+		fmt.Println(err)
 	}
-	tests := []struct {
-		name    string
-		g       graph
-		args    args
-		want    []int
-		wantErr bool
-	}{
-		{"error on invalid id", graph{}, args{1}, []int{}, true},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := tt.g.getNeighboursOf(tt.args.id)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("graph.getNeighboursOf() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("graph.getNeighboursOf() = %v, want %v", got, tt.want)
-			}
-		})
+	for _, id := range ids {
+		if _, ok := g[id]; !ok {
+			fmt.Printf("failed to retrieve nodes added")
+			t.Fail()
+		}
 	}
 }
 
-func exampleGraph() graph {
-	return graph{}
+func Test_Graph_AddEdges(t *testing.T) {
+	g := s.Graph{}
+	g.AddNodes([]int{1, 2, 3, 4, 5, 6, 99})
+	es := []s.Edge{
+		{1, 2}, {1, 3}, {1, 4},
+		{4, 99}, {4, 5}, {4, 6},
+		{99, 1}, {99, 2}, {99, 3},
+	}
+	g.AddEdges(es)
+	for _, e := range es {
+		if !checkEdge(g, e) {
+			fmt.Printf("check on edges failed on edge%v\n", e)
+			t.Fail()
+		}
+	}
+}
+
+func Test_Graph_AddEdges_directional(t *testing.T) {
+	g := s.Graph{}
+	g.AddNodes([]int{1, 2, 3, 4})
+	es := []s.Edge{{1, 2}, {2, 3}, {3, 4}, {4, 1}}
+	g.AddEdges_directional(es)
+	for _, e := range es {
+		if !checkEdge_directional(g, e) {
+			fmt.Printf("check on edges failed on edge%v\n", e)
+			t.Fail()
+		}
+	}
+}
+
+func Test_Graph_BredthFirst(t *testing.T) {
+
+	g := s.Graph{}
+	g.AddNodes([]int{1, 2, 3, 4, 5, 6})
+	g.AddEdges([]s.Edge{{1, 2}, {1, 3}, {2, 3}, {3, 4}, {3, 5}, {5, 6}})
+	path := g.BredthFirst(1, 2)
+	want := []int{1, 2}
+	if !reflect.DeepEqual(path, want) {
+		t.Errorf("wanted: %v\ngot: %v\n", want, path)
+	}
+
+	g = s.Graph{}
+	g.AddNodes([]int{1, 2, 3, 4, 5, 6})
+	g.AddEdges([]s.Edge{{1, 2}, {1, 3}, {2, 3}, {3, 4}, {3, 5}, {5, 6}})
+	path = g.BredthFirst(1, 4)
+	want = []int{1, 3, 4}
+	if !reflect.DeepEqual(path, want) {
+		t.Errorf("wanted: %v\ngot: %v\n", want, path)
+	}
+
+	g = s.Graph{}
+	g.AddNodes([]int{1, 2, 3, 4, 5, 6})
+	g.AddEdges([]s.Edge{{1, 2}, {1, 3}, {2, 3}, {3, 4}, {3, 5}, {5, 6}})
+	path = g.BredthFirst(5, 2)
+	want = []int{5, 3, 2}
+	if !reflect.DeepEqual(path, want) {
+		t.Errorf("wanted: %v\ngot: %v\n", want, path)
+	}
+	// directional edges
+	g = s.Graph{}
+	g.AddNodes([]int{1, 2, 3, 4})
+	g.AddEdges_directional([]s.Edge{{1, 2}, {2, 3}, {3, 4}, {4, 1}})
+	path = g.BredthFirst(2, 1)
+	want = []int{2, 3, 4, 1}
+	if !reflect.DeepEqual(path, want) {
+		t.Errorf("wanted: %v\ngot: %v\n", want, path)
+	}
+}
+
+func Test_Graph_GetNeighboursFor(t *testing.T) {
+	g := s.Graph{}
+	g.AddNodes([]int{1, 2, 3, 4, 5, 6, 99})
+	es := []s.Edge{
+		{1, 2}, {1, 3}, {1, 4},
+		{4, 99}, {4, 5}, {4, 6},
+		{99, 1}, {99, 2}, {99, 3},
+	}
+	g.AddEdges(es)
+	got := g.GetNeighboursFor(99)
+	wants := []int{1, 2, 3, 4}
+	for _, w := range wants {
+		if !sliceContainsInt(got, w) {
+			t.Errorf("Edge to (%d) was missing", w)
+		}
+	}
+}
+
+func sliceContainsInt(slice []int, i int) bool {
+	for _, si := range slice {
+		if si == i {
+			return true
+		}
+	}
+	return false
+}
+
+func checkEdge(g s.Graph, e s.Edge) bool {
+	if !sliceContainsInt(g.GetNeighboursFor(e[0]), e[1]) {
+		return false
+	}
+	return sliceContainsInt(g.GetNeighboursFor(e[1]), e[0])
+}
+
+func checkEdge_directional(g s.Graph, e s.Edge) bool {
+	return sliceContainsInt(g.GetNeighboursFor(e[0]), e[1])
 }
