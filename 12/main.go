2bddb57350269116e03817601b2025f592436b7f
Johan Strand <53336235+johan-st@users.noreply.github.com>
2022-12-19 01:07:22 +0100

    complete day 12 part 1

--- 12/main.go
+++ 12/main.go
@@ -6,73 +6,54 @@ import (
 
 	// u "github.com/johan-st/advent-of-code-2022/util"
 	u "github.com/johan-st/advent-of-code-2022/util"
+	// search "github.com/johan-st/advent-of-code-2022/util/search"
+	s "github.com/johan-st/advent-of-code-2022/util/search"
 )
 
 func main() {
-	heightMap := mapFromString(u.Load("sample.txt"))
-	// fmt.Printf("main %p\n", &heightMap)
-	fmt.Println(heightMap)
-	path := search(&heightMap)
-	fmt.Println(path)
-}
-func search(h *heightMap) []pos {
-	// fmt.Printf("search %p\n", h)
-	q := que{}
-	fmt.Printf("que search %p\n", &q)
-	q.enque(h.start)
-	prev := h.start
-	(*h.nodes)[prev.row][prev.col].dist = -1
-	for !q.empty() {
-		dist := (*h.nodes)[prev.row][prev.col].dist + 1
-		fmt.Printf("\ndist:%d\n", dist)
-		fmt.Printf("que:%d\n", len(q))
-		fmt.Printf("before visit (ext):%p\n", h.nodes)
-		visit(h, prev, dist, q.peek())
-		fmt.Printf("after visit (ext):%p\n", h.nodes)
-		prev = q.peek()
-		explore(&q, h)
-		fmt.Printf("que post exp:%d\n", len(q))
-
-		// TODO: debug
-		if dist > 1 {
-			fmt.Println("failed to find end within reasonable distance")
-			fmt.Println(h)
-			break
-		}
+	heightMap := mapFromString(u.Load("input.txt"))
+	// fmt.Println(heightMap)
+	graph, start, end := graphFromMap(heightMap)
+	shortestPath := graph.BredthFirst(start, end)
+	if len(shortestPath) < 2 {
+		fmt.Println("failed to establish path")
+	} else {
+		fmt.Println("shortest path is", len(shortestPath)-1, "steps")
 	}
-	return backtrack(*h)
 }
+func graphFromMap(m heightMap) (s.Graph, int, int) {
+	g := s.Graph{}
 
-func visit(h *heightMap, prev pos, dist int, p pos) {
-	// fmt.Printf("visit %p\n", h)
-	fmt.Println("visiting:", p, " prev:", prev)
-	fmt.Printf("before visit (int): %p\n", h.nodes)
-	(*h.nodes)[p.col][p.row].dist = dist
-	(*h.nodes)[p.col][p.row].predecessor = prev
-	(*h.nodes)[p.col][p.row].visited = true
-	fmt.Printf("after visit (int): %p\n", h.nodes)
-
-}
-
-func explore(q *que, h *heightMap) {
-	// fmt.Printf("que explore %p\n", q)
-	fmt.Printf("explore:%v\n", q.peek())
-	p := q.deque()
-	nps := validNeighbours(*h, p)
-	fmt.Printf("validNeighbours:%v\n", nps)
-	for _, np := range nps {
-		areDifferent := np.col != p.col || np.row != p.row
-		// fmt.Println("neighbour:", np.row, np.col, "prev:", p.row, p.col, "areDifferent:", areDifferent)
-		if areDifferent {
-			(*h.nodes)[np.row][np.col].predecessor = p
-			(*h.nodes)[np.row][np.col].dist = (*h.nodes)[p.row][p.col].dist + 1
-			q.enque(np)
-			if np.col == h.end.col && np.row == h.end.row {
-				fmt.Printf("--FOUND END--")
-				return
+	count := 0
+	edges := []s.Edge{}
+	start := m.start.row*len((*m.nodes)[0]) + m.start.col + 1
+	end := m.end.row*len((*m.nodes)[0]) + m.end.col + 1
+	for i, row := range *m.nodes {
+		for j := range row {
+			count++
+			ns := validNeighbours(m, pos{i, j})
+			for _, n := range ns {
+				idN := n.row*len(row) + n.col + 1
+				edges = append(edges, s.Edge{count, idN})
 			}
+
 		}
 	}
+
+	nodes := []int{}
+	for id := 1; id <= count; id++ {
+		nodes = append(nodes, id)
+	}
+
+	err := g.AddNodes(nodes)
+	if err != nil {
+		panic(err)
+	}
+	err = g.AddEdges_directional(edges)
+	if err != nil {
+		panic(err)
+	}
+	return g, start, end
 }
 
 func validNeighbours(h heightMap, p pos) []pos {
@@ -82,7 +63,7 @@ func validNeighbours(h heightMap, p pos) []pos {
 	// north
 	r := p.row - 1
 	c := p.col
-	if r >= 0 && !(*h.nodes)[r][c].visited {
+	if r >= 0 {
 		h := (*h.nodes)[r][c].height
 		if h <= n.height+1 {
 			ps = append(ps, pos{r, c})
@@ -93,7 +74,7 @@ func validNeighbours(h heightMap, p pos) []pos {
 	// south
 	r = p.row + 1
 	c = p.col
-	if r < len((*h.nodes))-1 && !(*h.nodes)[r][c].visited {
+	if r < len((*h.nodes)) {
 		h := (*h.nodes)[r][c].height
 		if h <= n.height+1 {
 			ps = append(ps, pos{r, c})
@@ -103,7 +84,7 @@ func validNeighbours(h heightMap, p pos) []pos {
 	// west
 	r = p.row
 	c = p.col - 1
-	if c >= 0 && !(*h.nodes)[r][c].visited {
+	if c >= 0 {
 		h := (*h.nodes)[r][c].height
 		if h <= n.height+1 {
 			ps = append(ps, pos{r, c})
@@ -113,34 +94,13 @@ func validNeighbours(h heightMap, p pos) []pos {
 	// east
 	r = p.row
 	c = p.col + 1
-	if c < len((*h.nodes)[r])-1 && !(*h.nodes)[r][c].visited {
+	if c < len((*h.nodes)[r]) {
 		h := (*h.nodes)[r][c].height
 		if h <= n.height+1 {
 			ps = append(ps, pos{r, c})
 		}
 	}
 	return ps
-}
-
-func backtrack(h heightMap) []pos {
-	return []pos{}
-}
-
-type que []pos
-
-func (q *que) enque(p pos) {
-	*q = append(*q, p)
-}
-func (q *que) deque() pos {
-	first := (*q)[0]
-	*q = (*q)[1:]
-	return first
-}
-func (q *que) empty() bool {
-	return len(*q) == 0
-}
-func (q *que) peek() pos {
-	return (*q)[0]
 }
 
 func mapFromString(str string) heightMap {
@@ -174,7 +134,7 @@ func runeToHeight(r rune) int {
 }
 
 func newNode(h int, p pos) node {
-	return node{height: h, visited: false, position: p}
+	return node{h}
 }
 
 func heightToRune(i int) rune {
@@ -182,11 +142,7 @@ func heightToRune(i int) rune {
 }
 
 type node struct {
-	position    pos
-	height      int
-	predecessor pos
-	dist        int
-	visited     bool
+	height int
 }
 
 type heightMap struct {
--- 12/main_test.go
+++ 12/main_test.go
@@ -4,41 +4,105 @@ import (
 	"fmt"
 	"reflect"
 	"testing"
+
+	s "github.com/johan-st/advent-of-code-2022/util/search"
 )
 
-func Test_que(t *testing.T) {
-	q := que{}
-	ps := []pos{{1, 2}, {2, 3}, {3, 4}}
-	if !q.empty() {
-		fmt.Println("new que was not empty")
+func Test_graphFromMap(t *testing.T) {
+	heightMap := mapFromString(sampleString())
+	g, s, e := graphFromMap(heightMap)
+	if s != 1 {
+		fmt.Println("starting position is wrong")
 		t.Fail()
 	}
-	q.enque(ps[0])
-	if q.empty() {
-		fmt.Println("que with element was empty")
+	if e != 22 {
+		fmt.Println("end position is wrong")
 		t.Fail()
 	}
-	if !reflect.DeepEqual(q.deque(), ps[0]) {
-		fmt.Println("dequed element was not the same as the qued")
+
+	if reflect.DeepEqual(g, sampleGraph()) {
+		fmt.Printf("%v\n", sampleGraph())
 		t.Fail()
 	}
-	q.enque(ps[0])
-	q.enque(ps[1])
-	q.enque(ps[2])
-	if !reflect.DeepEqual(q.peek(), ps[0]) {
-		fmt.Printf("peeked at element was not the same as the qued, first, \nexpected:%v\ngot:%v\n", ps[0], q.peek())
-		t.Fail()
+}
+
+func sampleString() string {
+	return "Sabqponm\r\nabcryxxl\r\naccszExk\r\nacctuvwj\r\nabdefghi"
+}
+func sampleGraph() s.Graph {
+	g := s.Graph{}
+	ids := []int{}
+	for id := 1; id <= 40; id++ {
+		ids = append(ids, id)
 	}
-	if !reflect.DeepEqual(q.deque(), ps[0]) {
-		fmt.Println("dequed element was not the same as the qued, first")
-		t.Fail()
+	g.AddNodes(ids)
+	g.AddEdges_directional(
+		[]s.Edge{
+			{1, 9}, {1, 2}, {2, 10},
+			{2, 1}, {2, 3},
+			{3, 11}, {3, 2},
+			{4, 12}, {4, 3}, {4, 5},
+			{5, 4}, {5, 6},
+			{6, 5}, {6, 7},
+			{7, 6},
+			{8, 16}, {8, 7},
+			{9, 1}, {9, 17}, {9, 10},
+			{10, 2}, {10, 18}, {10, 9}, {10, 11},
+			{11, 3}, {11, 19}, {11, 10},
+			{12, 4}, {12, 20}, {12, 11},
+			{13, 5}, {13, 21}, {13, 12}, {13, 14},
+			{14, 6}, {14, 13}, {14, 15},
+			{15, 7}, {15, 23}, {15, 14},
+			{16, 8}, {16, 24},
+			{17, 9}, {17, 25},
+			{18, 10}, {18, 26}, {18, 17}, {18, 19},
+			{19, 11}, {19, 27}, {19, 18},
+			{20, 12}, {20, 28}, {20, 19},
+			{21, 13}, {21, 29}, {21, 20}, {21, 22},
+			{22, 14}, {22, 30}, {22, 21}, {22, 23},
+			{23, 15}, {23, 31},
+			{24, 16}, {24, 32},
+			{25, 17},
+			{26, 18}, {26, 25}, {26, 27},
+			{27, 19}, {27, 26},
+			{28, 20}, {28, 27}, {28, 29},
+			{29, 28}, {29, 30},
+			{30, 29}, {30, 31},
+			{31, 23}, {31, 30},
+			{32, 24},
+			{33, 25}, {33, 34},
+			{34, 26}, {34, 33},
+			{35, 27}, {35, 34}, {35, 36},
+			{36, 35}, {36, 37},
+			{37, 36}, {37, 38},
+			{38, 37}, {38, 39},
+			{39, 38},
+			{40, 32}, {40, 39}},
+	)
+	return g
+}
+
+func Test_validNeighbours(t *testing.T) {
+	type args struct {
+		h heightMap
+		p pos
 	}
-	if !reflect.DeepEqual(q.deque(), ps[1]) {
-		fmt.Println("dequed element was not the same as the qued, second")
-		t.Fail()
+	tests := []struct {
+		name string
+		args args
+		want []pos
+	}{
+		{"first position", args{mapFromString(sampleString()), pos{0, 0}}, []pos{{1, 0}, {0, 1}}},
+		{"last position", args{mapFromString(sampleString()), pos{4, 7}}, []pos{{3, 7}, {4, 6}}},
+		{"39 position", args{mapFromString(sampleString()), pos{4, 6}}, []pos{{4, 5}, {4, 7}}},
+		{"top/end", args{mapFromString(sampleString()), pos{2, 4}}, []pos{{1, 4}, {3, 4}, {2, 3}, {2, 5}}},
+		// {"new", args{mapFromString(sampleString()), pos{}}, []pos{{}, {}}},
 	}
-	if !reflect.DeepEqual(q.deque(), ps[2]) {
-		fmt.Println("dequed element was not the same as the qued, third")
-		t.Fail()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := validNeighbours(tt.args.h, tt.args.p); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("validNeighbours(hm, %v) = %v, want %v", tt.args.p, got, tt.want)
+			}
+		})
 	}
 }
